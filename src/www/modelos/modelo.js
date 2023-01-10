abd1816c6ce73907274414c8d2565727972d4595
Juan Manuel Rincón <jrinconnavarro.guadalupe@alumnado.fundacionloyola.net>
2023-01-10 20:03:44 +0100

    añado logo, primer mockup, guia de estilos, fuentes, etc.

---
+++ src/www/modelos/modelo.js
@@ -0,0 +1,14 @@
+"use strict" //activo modo estricto
+/**
+ * Clase Modelo que gestiona los datos de la web
+ * */ 
+export class Modelo{
+	/**
+	 * 
+	 * @param {Object} controlador para que el modelo mire al controlador
+	 */
+	constructor(controlador) {
+		this.controlador=controlador
+        
+	}
+}
